26551cce4535fbaaba7e58ecdd942918d5967bcd
safrapc <leandrosafra.jconectada@gmail.com>
2021-09-26 23:21:16 -0300

    fix stabilish db

--- C/main.c
+++ C/main.c
@@ -11,14 +11,24 @@ int main()
     static char *unix_socket = NULL;
     int flag = 0;
 
+    //database connection stabilized
     MYSQL *conn;
-
+    //init mysql connection
     conn = mysql_init(NULL);
+
+    //verify if connection its nul for exit program
+    if (conn == NULL){
+    fprintf(stderr, "%s\n", mysql_error(conn));
+    exit(1);
+    }
+    //connect to database specified
     if(!(mysql_real_connect(conn,host,user,password,database,port,unix_socket,flag))){
         fprintf(stderr,"Error %s [%d]\n",mysql_error(conn),mysql_errno(conn));
         exit(1);
     }
-    printf("DATABASE CONNECTED!!");
+    //If we arrived here, the connection is ok and we can make mysql querys!
+    printf("Database connection stabilized\n\n");
+
 
     return 0;
 }
