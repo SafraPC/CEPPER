eb4db819a1bcbfbe3e656c99c3de905e1ac8bdfc
safrapc <leandrosafra.jconectada@gmail.com>
2021-10-18 19:35:38 -0300

    feat: create failOver and setTimeout methods

--- C/main.c
+++ C/main.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <mysql/mysql.h>
+#include <time.h>
+
 //try catch buffer
 int main()
 {
     const char *host = "127.0.0.1";
     const char *user = "root";
     const char *password = "admin";
-    const char *database = "testedb";
+    const char *database = "cepper";
     const int port = 3306;
     static char *unix_socket = NULL;
     const int flag = 0;
@@ -32,7 +34,8 @@ int main()
     //If we arrived here, the connection is ok and we can make mysql querys!
     printf("Database contectado..\n\n");
 
-    bool selectQuery(char *query)
+    //method for make sql query
+    bool cepperQuery(char *query)
     {
         //Making query..
         if (mysql_query(conn, query))
@@ -43,7 +46,7 @@ int main()
         //Store query result
         MYSQL_RES *result = mysql_store_result(conn);
         int num_fields = mysql_num_fields(result);
-        printf("Num fields :  %i", num_fields);
+        printf("Num fields :  %i\n", num_fields);
         //Get row for show data.
         MYSQL_ROW row;
         //feching rows in while, show data
@@ -52,7 +55,7 @@ int main()
             //while i its less than num_fields, show for us.
             for (int i = 0; i < num_fields; i++)
             {
-                printf("%s ", row[i] ? row[i] : "NULL");
+                printf("%s  line : %i\n", row[i] ? row[i] : "NULL",i);
             }
 
             printf("\n");
@@ -64,7 +67,49 @@ int main()
         return true;
     }
 
-    selectQuery("select * from test");
+    //timeout for C wait a little (special for request)
+    void setTimeout(int milliSec)
+    {
+        if (milliSec <= 0)
+        {
+            return;
+        }
+        int sinceMilli = clock() * 1000 / CLOCKS_PER_SEC;
+        int clockEnd = sinceMilli + milliSec;
+        do
+        {
+            sinceMilli = clock() * 1000 / CLOCKS_PER_SEC;
+        } while (sinceMilli <= clockEnd);
+    }
 
+    //failover if database it's over
+    bool failOverQuery(char *query, int thetries)
+    {
+        int tries = 0;
+        if(thetries > 0){
+            tries = thetries;
+        }
+        if (tries <= 7)
+        {
+            if (!cepperQuery(query))
+            {
+                tries = tries + 1;
+                setTimeout(1000);
+                failOverQuery(query,tries);
+            }
+            else
+            {
+                return true;
+            }
+        }else{
+            return false;
+        }
+    }
+    
+    if(failOverQuery("select * from usersssss",0)){
+        printf("Query executada com sucesso!");
+    }else{
+        printf("Não foi possível executar a query...");
+    }
     return 0;
 }
